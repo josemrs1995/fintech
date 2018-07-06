f4f97d814692338cf587a61b15c1a88a3042fd86
Your josemrs1995 <josemrs1995@gmail.com>
2018-07-06 17:20:23 -0400

    letras tamaño y demas

--- bedrock/web/app/themes/fintech-edux-institute/resources/views/partials/cursoslist.blade.php
+++ bedrock/web/app/themes/fintech-edux-institute/resources/views/partials/cursoslist.blade.php
@@ -4,7 +4,7 @@
         @foreach ($cursos_loop as $cursos)
         <div class="curso column is-3">
             <div class="colums">
-                <div class="img-container" style="background:url('{{$cursos['thumbnail']}}'); background-size:cover; height: 250px;">
+                <div class="img-container" style="background:url('{{$cursos['thumbnail']}}'); background-size: contain;">
                 </div>
                 <h2>{{$cursos['title']}}</h2>
                 <p>{!! $cursos['resumen'] !!}</p>
--- bedrock/web/app/themes/fintech-edux-institute/resources/views/partials/servicios.blade.php
+++ bedrock/web/app/themes/fintech-edux-institute/resources/views/partials/servicios.blade.php
@@ -2,8 +2,8 @@
     <div class="contenido-servicios column">
         <div class="columns is-multiline">
             @foreach ($servicios_loop as $servicios )
-                <div class="column is-4">
-                    <div class="img-serv" style="background:url('{{$servicios['thumbnail']}}'); background-size:cover; height: 250px;">
+                <div class="servicio-item column is-4">
+                    <div class="img-serv" style="background:url('{{$servicios['thumbnail']}}'); background-size: contain;">
                     </div>
                     <h2>{{$servicios['title']}}</h2>
                     <p>{!! $servicios['resumen'] !!}</p>
@@ -13,8 +13,8 @@
         <a class="boton" href="{{$servicios['link']}}">Ver Servicios</a>
         
     </div>
-    <div class="img-servicios column">
-        <h1>hola mundo</h1>
+    <div class="img-servicios column" style="background:url('{{ home_url('/app/uploads/2018/07/foto3.jpeg') }}'); background-size: cover;">
+    <img src="">
     </div>
     
 </div>
