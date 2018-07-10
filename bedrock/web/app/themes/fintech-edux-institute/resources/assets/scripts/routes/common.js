76bdb1ddb3107de082d9f0d45b9951e7faf3b361
Your josemrs1995 <josemrs1995@gmail.com>
2018-07-10 17:33:24 -0400

    menu-responseve

--- bedrock/web/app/themes/fintech-edux-institute/resources/assets/scripts/routes/common.js
+++ bedrock/web/app/themes/fintech-edux-institute/resources/assets/scripts/routes/common.js
@@ -1,6 +1,11 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    $('.navbar-burger').click(function (e) { 
+      e.preventDefault();
+      $('.menu-responsive').fadeToggle();
+      $('.navbar-burger').toggleClass('is-active');
+    });
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
