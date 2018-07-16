c9f85b5e1b3df7e9b4b6771432fcf2c668df98ca
Your josemrs1995 <josemrs1995@gmail.com>
2018-07-16 18:41:42 -0400

    cursos y talleres listos

--- bedrock/web/app/themes/fintech-edux-institute/resources/views/partials/cursoslist.blade.php
+++ bedrock/web/app/themes/fintech-edux-institute/resources/views/partials/cursoslist.blade.php
@@ -3,7 +3,7 @@
     <div class="columns conti">   
         @foreach (array_slice($cursos_loop,0,4) as $cursos)
         <div class="curso column is-3">
-            <div class="colums">
+            <div class="cursos-contenido">
                 <div class="img-container" style="background:url('{{$cursos['thumbnail']}}'); background-size: contain;">
                 </div>
                 <h2>{{$cursos['title']}}</h2>
