785868c6d880f79603541fc402be48b8067558f4
Your josemrs1995 <josemrs1995@gmail.com>
2018-08-03 16:51:34 -0400

    grafico listos

--- bedrock/web/app/themes/fintech-edux-institute/resources/views/partials/graficos.blade.php
+++ bedrock/web/app/themes/fintech-edux-institute/resources/views/partials/graficos.blade.php
@@ -1,8 +1,8 @@
-<div class="podcast-home columns">
-    <div class="podcast-contenido column" >
+<div class="grafico-home columns">
+    <div class="grafico-contenido column" >
         <iframe id="tradingview_55b89" src="https://s.tradingview.com/widgetembed/?frameElementId=tradingview_55b89&amp;symbol=SPX&amp;interval=D&amp;hidesidetoolbar=0&amp;symboledit=1&amp;saveimage=1&amp;toolbarbg=f1f3f6&amp;studies=%5B%5D&amp;theme=White&amp;timezone=UTC-4%2C5&amp;withdateranges=1&amp;studies_overrides=%7B%7D&amp;overrides=%7B%7D&amp;enabled_features=%5B%5D&amp;disabled_features=%5B%5D&amp;utm_source=www-tecnologiafinanciera-com.filesusr.com&amp;utm_medium=widget&amp;utm_campaign=chart&amp;utm_term=SPX" style="width: 100%; height: 100%; margin: 0 !important; padding: 0 !important;" frameborder="0" allowtransparency="true" scrolling="no" allowfullscreen=""></iframe>
     </div>
-    <div class="conte-podcast column">
+    <div class="conte-grafico column">
         <h1>{{ get_field('titulo-grafico', 'options') }}</h1>
         <h2>{{ get_field('subtitulo-grafico', 'options') }}</h2>
         <p>{{ get_field('contenido-grafico', 'options') }}</p>
